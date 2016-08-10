2536851b0f8ec5ccabf6a82e81022a675972763d
Michael Teagle <mkteagle@gmail.com>
2016-08-09 23:34:17 -0600

    made some great changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 (function () {
 	var gulp = require('gulp'),
-		browserSync = require('browser-sync'),
 		sass = require('gulp-ruby-sass'),
 		autoprefixer = require('gulp-autoprefixer'),
 		cssnano = require('gulp-cssnano'),
@@ -15,32 +14,26 @@
 		nodemon = require('gulp-nodemon'),
 		open = require('gulp-open'),
 		clean = require('gulp-clean'),
-		supervisor = require('gulp-supervisor'),
 		del = require('del');
 
-	gulp.task('delete', function () {
-		return gulp.src('public/', {read: false})
-			.pipe(clean());
-	});
-	gulp.task('styles', function() {
-		return sass('app/assets/styles/main.scss', { style: 'expanded' })
+	gulp.task('html', ['delete'], function() {
+		gulp.src(['app/donut-clicker/www/**/*'])
+			.pipe(gulp.dest('./public/donutclicker'));
+		gulp.src(['app/assets/thirdparty/**/*'])
+			.pipe(gulp.dest('./public/assets/thirdparty'));
+		sass('app/assets/styles/main.scss', { style: 'expanded' })
 			.pipe(autoprefixer('last 2 version'))
 			.pipe(gulp.dest('public/assets/styles'))
 			.pipe(rename({suffix: '.min'}))
 			.pipe(cssnano())
 			.pipe(gulp.dest('public/assets/styles'));
-	});
-	gulp.task('addcss', function() {
-		return sass('app/assets/styles/main.scss', { style: 'expanded' })
+		sass('app/assets/styles/main.scss', { style: 'expanded' })
 			.pipe(autoprefixer('last 2 version'))
 			.pipe(gulp.dest('app/assets/styles'))
 			.pipe(rename({suffix: '.min'}))
 			.pipe(cssnano())
 			.pipe(gulp.dest('app/assets/styles'));
-	});
-
-	gulp.task('scripts', function() {
-		return gulp.src('app/assets/scripts/**/*.js')
+		gulp.src('app/assets/scripts/**/*.js')
 			.pipe(jshint('.jshintrc'))
 			.pipe(jshint.reporter('default'))
 			.pipe(concat('main.js'))
@@ -48,38 +41,31 @@
 			.pipe(rename({suffix: '.min'}))
 			.pipe(uglify())
 			.pipe(gulp.dest('public/assets/scripts'));
-	});
-
-	gulp.task('images', function() {
-		return gulp.src('app/assets/img/**/*')
+		gulp.src('app/assets/img/**/*')
 			.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
 			.pipe(gulp.dest('public/assets/img'));
-	});
-
-	gulp.task('html', function() {
 		gulp.src('app/*.html')
 			.pipe(gulp.dest('public/'));
 		gulp.src('app/*.js')
 			.pipe(gulp.dest('public/'));
-		gulp.src(['app/assets/templates/**/*'])
-			.pipe(gulp.dest('./public/assets/templates'));
-		gulp.src(['app/assets/portfolio/**/*'])
-			.pipe(gulp.dest('./public/assets/portfolio'));
+		gulp.src('app/assets/templates/*.html')
+			.pipe(gulp.dest('public/assets/templates'));
+		gulp.src('app/assets/portfolio/*.html')
+			.pipe(gulp.dest('public/assets/portfolio'));
 	});
-	gulp.task('thirdparty', function(){
-		gulp.src(['app/assets/thirdparty/**/*'])
-			.pipe(gulp.dest('./public/assets/thirdparty'));
+	gulp.task('nodeman', [], function(){
+		nodemon({ script: 'public/server.js'
+			, ext: 'html js'
+			, ignore: ['ignored.js']
+			, tasks: [] })
+			.on('restart', function () {
+			});
 	});
-	gulp.task('donutclicker', function(){
-		gulp.src(['app/donut-clicker/www/**/*'])
-			.pipe(gulp.dest('./public/donutclicker'));
+	gulp.task('delete', function () {
+		return gulp.src('public/', {read: false})
+			.pipe(clean());
 	});
-	gulp.task('deploy', gulp.series('delete', 'thirdparty', 'html', 'scripts', 'styles', 'images', 'donutclicker', 'addcss'), function() {
-		supervisor('public/server.js');
-	});
-
-	gulp.task('develop', gulp.series('delete', 'styles', 'scripts', 'images', 'html', 'thirdparty', 'donutclicker', 'addcss'), function(){
-		supervisor('public/server.js');
+	gulp.task('develop', ['html','nodeman'], function(){
 	});
 
 
