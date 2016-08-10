bdb02f6034d5e9a2fc908ac9e09a85f6ee87c1db
Michael Teagle <mkteagle@gmail.com>
2016-08-09 23:34:33 -0600

    added some more changes to public folder

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,11 @@
+(function() {
+    angular.module('ngController', [])
+        .controller('ngController', ngController);
+    function ngController() {
+    	var self = this;
+	    self.options = false;
+	    self.dud = false;
+        
+    }
+
+}());
