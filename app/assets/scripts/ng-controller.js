6b1b53c822807359f4d8c8d4bdbb56786f50494d
mkteagle <mkteagle@gmail.com>
2016-09-06 00:21:40 -0600

    making responsive

--- app/assets/scripts/ng-controller.js
+++ app/assets/scripts/ng-controller.js
@@ -1,8 +1,8 @@
 (function() {
     angular.module('ngController', [])
         .controller('ngController', ngController);
-	ngController.$inject =['$http', '$window'];
-    function ngController($http, $window) {
+	ngController.$inject =['$http', '$window', '$state'];
+    function ngController($http, $window, $state) {
     	var self = this;
 	    self.options = false;
 	    self.dud = false;
@@ -13,6 +13,7 @@
 	    self.$http = $http;
 		self.donutclicker = donutclicker;
 		self.flappy = flappy;
+		self.home = home;
 
 	    function post() {
 	    	var message = {
@@ -28,6 +29,9 @@
 			    self.content = '';
 		    });
 	    }
+		function home() {
+			$state.go('home');
+		}
 		function donutclicker() {
 			$window.location.href = './donutclicker/index.html';
 		}
